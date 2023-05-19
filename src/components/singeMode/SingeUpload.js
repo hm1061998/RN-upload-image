e68acb836bb56cb41e13ceda5308f32dfef3b490
hm1061998 <hm1061998@gmail.com>
2023-05-19 09:35:36 +0700

    update version picker image

---
+++ src/components/singeMode/SingeUpload.js
@@ -0,0 +1,222 @@
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  forwardRef,
+} from "react";
+import UploadImageItem from "./UploadImageItem";
+import { TouchableOpacity, Platform, View, Text } from "react-native";
+import PropTypes from "prop-types";
+import uuid from "react-native-uuid";
+import Icon from "../Icon";
+// import * as ImageManipulator from 'expo-image-manipulator';
+import * as MultipleImagePicker from "@baronha/react-native-multiple-image-picker";
+
+const SingeUpload = ({
+  configOption,
+  configUrl,
+  value,
+  onChange,
+  mediaType,
+  onStartUpload,
+  onEndUpload,
+  sizeImage,
+  icon,
+  shape,
+}) => {
+  const [dataImage, setDataImage] = useState(value);
+
+  useEffect(() => {
+    setDataImage(value);
+  }, [value]);
+
+  // console.log({ value });
+
+  const openPicker = useCallback(async () => {
+    try {
+      const options = {
+        usedCameraButton: true,
+        singleSelectedMode: true,
+        doneTitle: "Xong",
+        cancelTitle: "Hủy",
+        maximumMessageTitle: "Thông báo",
+        tapHereToChange: "Chạm vào đây để thay đổi",
+        maximumMessage: "Bạn đã chọn số lượng phương tiện tối đa được phép",
+        isPreview: true,
+        mediaType,
+      };
+
+      const response = await MultipleImagePicker.openPicker(options);
+
+      const check = response?.path?.match(/:/);
+      let uriImage = response?.path;
+      if (!check) {
+        uriImage = `file://${uriImage}`;
+      }
+
+      const newItem = {
+        id: uuid.v4(),
+        localIdentifier: response?.localIdentifier,
+        status: "uploading",
+        path: uriImage,
+        name: response?.fileName,
+        type: response?.mime,
+      };
+
+      onStartUpload?.();
+      setDataImage(newItem);
+      uploadImage(newItem);
+    } catch (e) {
+      // console.log(e.code, e.message);
+    }
+  }, [mediaType, onStartUpload, uploadImage]);
+
+  const createFormData = (photo, body = {}) => {
+    const data = new FormData();
+    data.append("file", {
+      name: photo.name,
+      type: photo.type,
+      uri:
+        Platform.OS === "ios" ? photo.path.replace("file://", "") : photo.path,
+    });
+
+    Object.keys(body).forEach((key) => {
+      data.append(key, body[key]);
+    });
+
+    return data;
+  };
+
+  const uploadImage = useCallback(
+    async (file) => {
+      const fd = createFormData(file);
+      // fd.append('file', file);
+      const options = {
+        ...configOption,
+        body: fd,
+      };
+
+      // console.log('fd', fd);
+      let newData;
+      try {
+        const response = await fetch(configUrl, options).then((_response) => {
+          return _response.json();
+        });
+
+        // console.log({ response });
+
+        if (response?.filename) {
+          newData = {
+            id: file.id,
+            localIdentifier: file.localIdentifier,
+            name: response?.filename,
+            uri: response.path,
+            type: response?.mimetype,
+            status: "done",
+          };
+        } else {
+          // Alert.alert('upload error:' + JSON.stringify(response));
+          newData = {
+            ...file,
+            status: "failed",
+          };
+        }
+      } catch (e) {
+        newData = {
+          ...file,
+          status: "failed",
+        };
+      } finally {
+        onChange?.(newData);
+        onEndUpload?.(newData);
+
+        setDataImage(newData);
+      }
+    },
+    [configOption, configUrl, onChange, onEndUpload]
+  );
+
+  const renderData = useMemo(
+    () => (
+      <>
+        {dataImage && (
+          <UploadImageItem
+            data={dataImage}
+            sizeImage={sizeImage}
+            onDelete={() => {
+              setDataImage(null);
+              onChange?.(null);
+            }}
+          />
+        )}
+
+        {!dataImage && (
+          <TouchableOpacity activeOpacity={0.5} onPress={() => openPicker()}>
+            <View
+              style={[
+                {
+                  borderRadius: shape === "square" ? 4 : 9999,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#F9F9F9",
+                  borderWidth: 0.7,
+                  borderColor: "#CACACA",
+                  borderStyle: "dashed",
+                  width: sizeImage,
+                  height: sizeImage,
+                },
+              ]}
+            >
+              {icon ? (
+                icon()
+              ) : (
+                <Icon type="FontAwesome" name="user" size={50} color={"gray"} />
+              )}
+            </View>
+
+            <Text style={{ marginTop: 8, fontSize: 14, fontWeight: "500" }}>
+              Thêm ảnh đại diện
+            </Text>
+          </TouchableOpacity>
+        )}
+      </>
+    ),
+
+    [dataImage, sizeImage, shape, icon, onChange, openPicker]
+  );
+
+  return renderData;
+};
+
+SingeUpload.defaultProps = {
+  configOption: {
+    headers: {
+      "x-auth-key":
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYmFuaGFuZyIsInVzZXJJZCI6IjE4OCIsInVzZXJHcm91cHNJZCI6IjIiLCJpYXQiOjE2Mzc4MjkxMzIsImV4cCI6MTYzNzkxNTUzMn0.oyfXKs857Ywv7Btysf3TzQVbURCvLcQdt6pgL2u19B0",
+      "Content-Type": "multipart/form-data",
+      "x-auth-project": "choso",
+    },
+    method: "POST",
+  },
+  configUrl: "https://imgchoso.vgasoft.vn/files/uploadHandler",
+  maxLength: 3,
+  mediaType: "image",
+  isCheckSelected: false,
+  sizeImage: 120,
+};
+
+SingeUpload.propTypes = {
+  configOption: PropTypes.object,
+  configUrl: PropTypes.string,
+  onChange: PropTypes.func,
+  maxLength: PropTypes.number,
+  mediaType: PropTypes.oneOf(["all", "image", "video"]),
+  isCheckSelected: PropTypes.bool,
+  sizeImage: PropTypes.number,
+};
+
+const RootElement = (props, ref) => {
+  return <SingeUpload {...props} innerRef={ref} />;
+};
+export default forwardRef(RootElement);
---
+++ src/components/singeMode/UploadImageItem.js
@@ -0,0 +1,99 @@
+import React, { useMemo } from "react";
+import { View, TouchableOpacity, ActivityIndicator } from "react-native";
+import FastImage from "react-native-fast-image";
+import ImageViewer from "../ImageViewer";
+import Icon from "../Icon";
+// import Lightbox from 'react-native-lightbox-v2';
+
+const Index = ({ data, onDelete, sizeImage, convertLinkImg, disabled }) => {
+  const renderImage = useMemo(() => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          imgViewerRef.current.show([{ uri: data?.path || data.uri }]);
+        }}
+      >
+        <FastImage
+          style={[styles.img, { width: sizeImage, height: sizeImage }]}
+          source={{
+            uri: data?.path || convertLinkImg(data?.uri),
+          }}
+        />
+      </TouchableOpacity>
+    );
+  }, [data?.path, data?.uri, sizeImage]);
+  // console.log('render', getLinkImg(data?.path));
+  return (
+    <View style={[styles.boxImage, { width: sizeImage, height: sizeImage }]}>
+      {data?.status !== "uploading" && !disabled && (
+        <TouchableOpacity
+          style={styles.delete}
+          onPress={() => {
+            if (onDelete) {
+              onDelete(data);
+            }
+          }}
+        >
+          <Icon type="Ionicons" name="close-outline" size={13} color="#fff" />
+        </TouchableOpacity>
+      )}
+      {data?.status === "uploading" ? (
+        <View style={[styles.containerLoading]}>
+          <ActivityIndicator size="small" color="blue" />
+        </View>
+      ) : null}
+      {data?.status === "failed" ? (
+        <View style={[styles.containerLoading]}>
+          <Icon typr="Entypo" name="warning" size={20} color="red" />
+        </View>
+      ) : null}
+      {renderImage}
+      <ImageViewer ref={imgViewerRef} convertLinkImg={convertLinkImg} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  boxImage: {
+    borderRadius: 999,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 8,
+    marginBottom: 10,
+    backgroundColor: "#F9F9F9",
+    position: "relative",
+  },
+  img: {
+    borderRadius: 4,
+    borderWidth: 0.7,
+    borderColor: "#c7cad9",
+  },
+  containerLoading: {
+    position: "absolute",
+    zIndex: 10,
+    backgroundColor: "#ffffffa6",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  delete: {
+    position: "absolute",
+    top: -5,
+    right: -5,
+    width: 20,
+    height: 20,
+    backgroundColor: "gray",
+    borderRadius: 100,
+    zIndex: 11,
+    borderWidth: 1,
+    borderColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default React.memo(Index);
